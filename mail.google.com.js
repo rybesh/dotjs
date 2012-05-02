f253bd321a59ab85c109429c41ca64e1491fec2b
Ryan Shaw <ryanshaw@unc.edu>
2012-05-02 12:51:07 -0400

    Just poll instead of waiting for unreliable load event.

--- mail.google.com.js
+++ mail.google.com.js
@@ -1,17 +1,12 @@
 // Hide unread counts.
-var iframe, interval, links, text, count = / \(\d+\)/;
-$(function(){
-  $("#canvas_frame").load(function(){
-    iframe = $(this);
-    interval = setInterval(function() {
-      links = iframe.contents().find("div.aim a");
-      if (links.length > 0) { 
-        clearInterval(interval);
-        links.map(function() {
-          text = $(this).text();
-          $(this).text(text.replace(count, ""));
-        });
-      }
-    }, 100);
-  });
-});
+var interval, links, text, count = / \(\d+\)/;
+interval = setInterval(function() {
+  links = $("#canvas_frame").contents().find("div.aim a");
+  if (links.length > 0) { 
+    clearInterval(interval);
+    links.map(function() {
+      text = $(this).text();
+      $(this).text(text.replace(count, ""));
+    });
+  }
+}, 100);
